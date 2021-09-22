e06ab8230758ac102a3fd9499da5e90151d1aa91
rishusiva <rishusiva@gmail.com>
2021-09-22 16:28:53 +0530

    mediapipe detection

---
+++ Code/mediapipe_detection.py
@@ -0,0 +1,9 @@
+from modules import *
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+    image.flags.writeable = False                  
+    results = model.process(image)                 
+    image.flags.writeable = True                   
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) 
+    return image, results
