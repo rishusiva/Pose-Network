dbab6d49e40d4546d135a38d9fde05a1b2bd1c86
rishusiva <rishusiva@gmail.com>
2021-09-22 17:16:14 +0530

    visualization commit

---
+++ Code/visualization.py
@@ -0,0 +1,14 @@
+from modules import *
+
+
+
+colors = [(255,0,0), (0,255,0), (0,0,255)]
+
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
