8c1eaeab955df87e22f93e4566f3d2f4f008e0d8
rishusiva <rishusiva@gmail.com>
2021-09-22 16:45:32 +0530

    keypoints extraction

---
+++ Code/keypoints_extraction.py
@@ -0,0 +1,10 @@
+from modules import *
+from landmark_detection import results
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+    left_hand = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(63)
+    right_hand = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(63)
+    return np.concatenate([pose, face, left_hand, right_hand])
